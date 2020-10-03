5764df1f5b0abb0ea8a8ce8fc3c19bb5937a3a76
korn <korn@gmail.com>
2020-10-03 19:32:55 +0700

    res

---
+++ data.js
@@ -0,0 +1,63 @@
+const data = [
+    {
+        id:1,
+        name:"ไก่ไม่ยอมนอน - ซอยรามบุตรี",
+        type:"Coupon,Partner,ข้าวหน้า",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLDNAATVA2UNJ/hero/cab978976ec14ec3bffc196e49fcd919_1594554201623013383.png"
+    },
+    {
+        id:2,
+        name:"Kamu Tea (คามุ ที) - วังหลัง",
+        type:"Partner,ชานมไข่มุก",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZC1VN5CBEXGAT/hero/2cac19cd03f84037a8046cd3d9a30b85_1585793335979199515.jpeg"
+    },
+    {
+        id:3,
+        name:"ตี๋เย็นตาโฟรสเด็ด - ศาลาว่าการกรุงเทพฯ",
+        type:"Partner,อาหารเส้น",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZMTKBDTT2XJAJ/hero/65641d0fcdc14d4a8961f56f0ead6fa5_1598847900883974941.jpeg"
+    },
+    {
+        id:4,
+        name:"Hongkong Noodle (ฮ่องกงนู้ดเดิ้ล) - บางลำภู",
+        type:"Coupon,Partner,ข้าวหน้า,อาหารเส้น",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY6VLJMTEXB1TN/hero/69428a4b3ef444d8b5786a424c8c5bba_1590933632384074114.jpeg"
+    },
+    {
+        id:5,
+        name:"Yummy Cleanfood (ยัมมี่ คลีนฟู้ด) - ถนนมิตรพันธ์",
+        type:"Coupon,Partner,อาหารสุขภาพ",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000038e/hero/bd6535ebd40843b5be9fa1c837f1ff14_1587371265883225043.jpeg"
+    },
+    {
+        id:6,
+        name:"อ้อย ข้าวคลุกกะปิ - อรุณ​อัม​รินทร์",
+        type:"Partner,อาหารตามสั่ง",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZKTN2TYCTCEFA/hero/cb5b82ec7edb425daeabe9adf1fea062_1593683118171424592.jpeg"
+    },
+    {
+        id:7,
+        name:"ข้าวมันไก่ไหหลำนายจิว - ถนนดินสอ",
+        type:"Partner,อาหารตามสั่ง",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZE1VN5KVEVEGX/hero/5819f2072fed4be9a7a4ccbbc2198626_1590131373308341816.jpeg"
+    },
+    {
+        id:8,
+        name:"นายอ้วนเย็นตาโฟเสาชิงช้า - ถนนตะนาว",
+        type:"Coupon,Partner,อาหารเส้น",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000022y/hero/c4b9c9ca49f84274861dc367529d3cc0_1592216282560213295.jpeg"
+    },
+    {
+        id:9,
+        name:"ข้าวหมูทอด krit - ถนนศิริพงษ์",
+        type:"Partner,ข้าวหน้า",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZKZN8MVEUTJEA/hero/c3ab80aeebac42f59d41d3cf14a50504_1594112097107461277.jpeg"
+    },
+    {
+        id:10,
+        name:"บุงกี่ เป็ดย่างฮ่องกง - บางลำพู",
+        type:"Coupon,Partner,อาหารจีน",
+        imageURL:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY4CJ2WAKAAZFE/hero/eac950d310ee457685bb98acf537d7ac_1576573337301905327.jpeg"
+    },
+]
+module.exports =data;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const restaurantRouter = require('./routes/restaurant');
+const indexRouter = require('./routes/index');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.engine('hbs',hbs({extname:'hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/api',restaurantRouter);
+app.use('/',indexRouter);
+
+app.listen(
+    PORT,
+    () => {
+      console.log(`Listening to port ${PORT}`);
+    }
+  );
+  
